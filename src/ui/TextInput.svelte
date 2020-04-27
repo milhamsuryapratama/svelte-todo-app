a0a6057c87001ec8241fc03b5168b9e7f3590dff
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-27 13:32:41 +0700

    membuat TextInput component

---
+++ src/ui/TextInput.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let value;
+</script>
+
+<div class="col-md-6">
+  <form on:submit|preventDefault>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Todo</label>
+      <input
+        type="text"
+        class="form-control"
+        id="todo"
+        name="todo"
+        value={value === undefined ? '' : value}
+        aria-describedby="emailHelp"
+        on:input />
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
