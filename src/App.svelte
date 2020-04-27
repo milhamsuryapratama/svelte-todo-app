c67341dc12b2fd9377ede626086b5472fa606abe
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-27 13:50:55 +0700

    perubahan minor indentasi, import TextInput component dan fixing props

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,55 @@
 <script>
-	import todoStore from './store/todoStore.js';
-	import { onDestroy } from 'svelte';
-	import Header from './ui/Header.svelte';
-	import Tabel from './component/Data.svelte';
+  import todoStore from "./store/todoStore.js";
+  import { onDestroy } from "svelte";
+  import Header from "./ui/Header.svelte";
+  import Tabel from "./component/Data.svelte";
+  import TextInput from './ui/TextInput.svelte';
 
-	let todos;
+  let todos;
 
-	const unsubscribe = todoStore.subscribe(t => {
-		todos = t;
-	});
+  const unsubscribe = todoStore.subscribe(t => {
+    todos = t;
+  });
 
-	$: console.log(todoStore);
+  $: console.log(todoStore);
 
-	let todo;
-	let edited = false;
-	let editeId;
+  let todo;
+  let edited = false;
+  let editeId;
 
-	onDestroy(unsubscribe);
+  onDestroy(unsubscribe);
 
-	function submitForm(event) {
-		if (edited) {
-			todos[editeId] = event.target.todo.value;
-			todoStore.set(todos);
-		} else {
-			todoStore.update(t => {
-				return [...t, todo];
-			});
-		}
-		editeId = null;
-		edited = false;
-		todo = '';
-	}
+  function submitForm(event) {
+    if (edited) {
+      todos[editeId] = event.target.todo.value;
+      todoStore.set(todos);
+    } else {
+      todoStore.update(t => {
+        return [...t, todo];
+      });
+    }
+    editeId = null;
+    edited = false;
+    todo = "";
+  }
 
-	function hapusTodo(event) {
-		todos.splice(event.detail.id, 1);
-		todoStore.set(todos);
-	}
+  function hapusTodo(event) {
+    todos.splice(event.detail.id, 1);
+    todoStore.set(todos);
+  }
 
-	function editTodo(event) {
-		edited = true;
-		editeId = event.detail.id;
-		todo = todos[event.detail.id];
-	}
+  function editTodo(event) {
+    edited = true;
+    editeId = event.detail.id;
+    todo = todos[event.detail.id];
+  }
 </script>
 
-<Header/>
+<Header />
 
 <div class="row">
-	<div class="col-md-6">
-		<form on:submit|preventDefault={submitForm}>
-			<div class="form-group">
-				<label for="exampleInputEmail1">Todo</label>
-				<input type="text" class="form-control" id="todo" name="todo" aria-describedby="emailHelp" bind:value={todo}>
-			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-		</form>	
-	</div>
-
-	<div class="col-md-6">
-		<Tabel 
-			data={todos}
-			on:hapusTodo={hapusTodo}
-			on:editTodo={editTodo}
-			/>
-	</div>
+	<TextInput on:input={event => (todo = event.target.value)} on:submit={submitForm} value={todo} />
+  <div class="col-md-6">
+    <Tabel data={todos} on:hapusTodo={hapusTodo} on:editTodo={editTodo} />
+  </div>
 </div>
