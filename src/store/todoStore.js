20c28d614e286f9b05d9ec07cf59f0a92dfed89e
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-28 11:17:09 +0700

    buat event object dan export it

--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -4,4 +4,21 @@ const todos = writable([
     'Yolo', 'Jojo'
 ]);
 
-export default todos;
+const event = {
+    subscribe: todos.subscribe,
+    addTodo: todo => {
+        todos.update(t => {
+            return [...t, todo];
+        });
+    },
+    deleteTodo: id => {
+        todos.update(t => {
+            return t.splice(id, id);
+        });
+    },
+    updateTodo: todos => {
+        return todos.set(todos);
+    }
+}
+
+export default event;
