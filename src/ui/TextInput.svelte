53c242638b2e41471e691739aba6c667d28c943c
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-28 12:46:10 +0700

    fixing TextInpur component

--- src/ui/TextInput.svelte
+++ src/ui/TextInput.svelte
@@ -2,19 +2,13 @@
   export let value;
 </script>
 
-<div class="col-md-6">
-  <form on:submit|preventDefault>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Todo</label>
-      <input
-        type="text"
-        class="form-control"
-        id="todo"
-        name="todo"
-        value={value === undefined ? '' : value}
-        aria-describedby="emailHelp"
-        on:input />
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+<div class="form-group">  
+  <input
+    type="text"
+    class="form-control"
+    id="todo"
+    name="todo"
+    value={value === undefined ? '' : value}
+    aria-describedby="emailHelp"
+    on:input />
 </div>
